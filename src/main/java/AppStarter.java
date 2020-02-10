5f4dd9ab7f06c8132ec0afcc167bb3327aeb2bab
Alexey Afanasiev <megabochka@gmail.com>
2020-02-10 15:09:59 +0300

    Update AppStarter.java

--- src/main/java/AppStarter.java
+++ src/main/java/AppStarter.java
@@ -1,10 +1,11 @@
 public class AppStarter {
     public static void main(String[] args) {
-        int a ;
-        int b = a;
+        int c ;
+        
         if(true) {
 
         }
+        int d ;
         return;
     }
 }
