313cdb9a596f8a0cd0f42a2374ffe9cd96593ab4
Alexey Afanasiev <megabochka@gmail.com>
2020-04-09 16:39:10 +0300

    Update AppStarter.java

--- src/main/java/AppStarter.java
+++ src/main/java/AppStarter.java
@@ -1,10 +1,16 @@
 public class AppStarter {
+    @SuppressWarnings("StatementWithEmptyBody")
     public static void main(String[] args) {
         int a ;
         int b = a;
         if(true) {
 
         }
+        
+        //noinspection ConstantConditions
+    if (true){
+      
+    }
         return;
     }
 }
